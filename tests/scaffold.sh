9e02e3f915607b786627dc5f25924bfd57388173
sobolevn <mail@sobolevn.me>
2018-05-07 02:22:18 +0300

    Fixes expect env vars

--- tests/scaffold.sh
+++ tests/scaffold.sh
@@ -2,7 +2,7 @@
 
 set timeout 360
 
-spawn ./node_modules/.bin/vue init . "$PROJECT_NAME"
+spawn ./node_modules/.bin/vue init . "$env(PROJECT_NAME)"
 
 # This happens because of
 # https://github.com/vuejs/vue-cli/issues/291

b4fad269a6eaf60c62784d3233240e5b97ef66cc
sobolevn <mail@sobolevn.me>
2018-05-07 12:59:42 +0300

    Turning off deploys

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -3,31 +3,26 @@
 set -o errexit
 set -o nounset
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  # For some reason `deploy` key did not work for us:
-  # https://travis-ci.org/wemake-services/wemake-vue-template/builds/375684628
-  npm install -g now
+echo 'skipping deployment'
 
-  pwd
-
-  cd "$PROJECT_NAME"
-
-  pwd
-  cat "../now.json"
-  cat "config/.env"
-  echo "$NOW_TOKEN $PROJECT_NAME"
-
-  # Building new instance:
-  now deploy \
-    --token="$NOW_TOKEN" \
-    --no-clipboard \
-    --public \
-    --npm \
-    --dotenv=config/.env \
-    --local-config=../now.json
-
-  # Setting new instance as a default one:
-  now alias --token="$NOW_TOKEN" --local-config=../now.json
-else
-  echo 'skipping deployment'
-fi
+# if [ "$TRAVIS_BRANCH" = "master" ]; then
+#   # For some reason `deploy` key did not work for us:
+#   # https://travis-ci.org/wemake-services/wemake-vue-template/builds/375684628
+#   npm install -g now
+#
+#   cd "$PROJECT_NAME"
+#
+#   # Building new instance:
+#   now deploy \
+#     --token="$NOW_TOKEN" \
+#     --no-clipboard \
+#     --public \
+#     --npm \
+#     --dotenv=config/.env \
+#     --local-config=../now.json
+#
+#   # Setting new instance as a default one:
+#   now alias --token="$NOW_TOKEN" --local-config=../now.json
+# else
+#   echo 'skipping deployment'
+# fi

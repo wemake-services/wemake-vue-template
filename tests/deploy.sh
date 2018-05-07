533d6dd11dcb77ce834785014a813786139e5444
sobolevn <mail@sobolevn.me>
2018-05-07 11:01:27 +0300

    Returns deploy

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -3,16 +3,22 @@
 set -o errexit
 set -o nounset
 
-cd "$PROJECT_NAME"
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  # For some reason `deploy` key did not work for us:
+  # https://travis-ci.org/wemake-services/wemake-vue-template/builds/375684628
+  cd "$PROJECT_NAME"
 
-# Building new instance:
-now deploy \
-  --token="$NOW_TOKEN" \
-  --no-clipboard \
-  --public \
-  --npm \
-  --dotenv=config/.env \
-  --local-config=../now.json
+  # Building new instance:
+  now deploy \
+    --token="$NOW_TOKEN" \
+    --no-clipboard \
+    --public \
+    --npm \
+    --dotenv=config/.env \
+    --local-config=../now.json
 
-# Setting new instance as a default one:
-now alias --token="$NOW_TOKEN" --local-config=../now.json
+  # Setting new instance as a default one:
+  now alias --token="$NOW_TOKEN" --local-config=../now.json
+else
+  echo 'skipping deployment'
+fi

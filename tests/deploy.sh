95ebdc59fe7b317d55058b0af4356d9c1e290664
sobolevn <mail@sobolevn.me>
2018-05-07 11:08:18 +0300

    Fixes deployment

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -6,6 +6,8 @@ set -o nounset
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   # For some reason `deploy` key did not work for us:
   # https://travis-ci.org/wemake-services/wemake-vue-template/builds/375684628
+  npm install -g now
+
   cd "$PROJECT_NAME"
 
   # Building new instance:

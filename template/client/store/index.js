ae48611f69c82184a3f15cda410f2842747a3ff2
Kirill Vysokolov <kir2250@gmail.com>
2018-05-17 16:36:29 +0300

    changed store to classic variant

--- template/client/store/index.js
+++ template/client/store/index.js
@@ -2,6 +2,8 @@
 
 import type { ActionContext } from 'vuex'
 
+import Vue from 'vue'
+import Vuex from 'vuex'
 import comments from '~/logics/api/comments'
 import * as mutationTypes from '~/types/mutations'
 import type {
@@ -11,67 +13,67 @@ import type {
   RawCommentType
 } from '~/types'
 
-function state (): StateType {
-  return {
-    comments: null
-  }
-}
+Vue.use(Vuex)
 
-const getters = {
-  hasComments (state: StateType): boolean {
-    return Boolean(state.comments && state.comments.length > 0)
-  }
-}
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      comments: null
+    },
+    mutations: {
+      [mutationTypes.SET_COMMENTS]: (
+        state: StateType, comments: Array<RawCommentType>
+      ) => {
+        const updatedComments: Array<CommentType> = []
 
-const mutations = {
-  [mutationTypes.SET_COMMENTS]: (
-    state: StateType, comments: Array<RawCommentType>
-  ) => {
-    const updatedComments: Array<CommentType> = []
+        for (const comment of comments.slice(0, 10)) {
+          const newOne = {
+            ...comment,
+            rating: 0 // try to comment out this line
+          }
+          updatedComments.push(newOne)
+        }
 
-    for (const comment of comments.slice(0, 10)) {
-      const newOne = {
-        ...comment,
-        rating: 0 // try to comment out this line
+        state.comments = updatedComments
+      },
+
+      [mutationTypes.UPDATE_RATING]: (
+        state: StateType, {
+          commentId,
+          delta
+        }: CommentPayloadType
+      ) => {
+        if (!state.comments) return
+
+        const commentIndex = state.comments.findIndex((c: CommentType) => {
+          return c.id === commentId
+        })
+
+        if (!state.comments || !state.comments[commentIndex]) return
+
+        state.comments[commentIndex].rating += delta
       }
-      updatedComments.push(newOne)
+    },
+    getters: {
+      hasComments(state: StateType): boolean {
+        return Boolean(state.comments && state.comments.length > 0)
+      }
+    },
+    actions: {
+      async fetchComments({
+        commit,
+        state
+      }: ActionContext<StateType>) {
+        const data = await comments.fetchComments(this.$axios)
+        commit(mutationTypes.SET_COMMENTS, data)
+
+        // Uncomment next line to see typing in action:
+        // console.log(state.comments, state.fake)
+
+        return data
+      }
     }
-
-    state.comments = updatedComments
-  },
-
-  [mutationTypes.UPDATE_RATING]: (
-    state: StateType, { commentId, delta }: CommentPayloadType
-  ) => {
-    if (!state.comments) return
-
-    const commentIndex = state.comments.findIndex((c: CommentType) => {
-      return c.id === commentId
-    })
-
-    if (!state.comments || !state.comments[commentIndex]) return
-
-    state.comments[commentIndex].rating += delta
-  }
+  })
 }
 
-const actions = {
-  async fetchComments (
-    { commit, state }: ActionContext<StateType>
-  ) {
-    const data = await comments.fetchComments(this.$axios)
-    commit(mutationTypes.SET_COMMENTS, data)
-
-    // Uncomment next line to see typing in action:
-    // console.log(state.comments, state.fake)
-
-    return data
-  }
-}
-
-export {
-  state,
-  getters,
-  mutations,
-  actions
-}
+export default createStore

d723fdd81a72e9b6345b5a7e533f60d94988ef94
sobolevn <mail@sobolevn.me>
2018-05-24 12:08:27 +0300

    Adds stricter jest linting, closes #529, closes #528

---
+++ template/tests/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'extends': ['plugin:jest/recommended'],
+  'rules': {
+    'jest/consistent-test-it': ['error', { 'fn': 'test' }],
+    'jest/lowercase-name': ['error'],
+    'jest/valid-describe': 'error',
+    'jest/no-jest-import': 'error',
+    'jest/prefer-to-have-length': 'error',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-be-undefined': 'error',
+    'jest/valid-expect-in-promise': 'error',
+
+    // warn users about too large snapshots:
+    'jest/no-large-snapshots': ['warn', { 'maxSize': 50 }]
+
+    // we don't use this rule, since we use `babel-jest-assertions`
+    // which inserts `expect.assertions()` automatically
+    // 'jest/prefer-expect-assertions': 'error'
+  }
+}

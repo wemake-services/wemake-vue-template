27753327a2e9b0ca3563e620bae411f7ed8d73c3
sobolevn <mail@sobolevn.me>
2018-08-05 13:43:25 +0300

    Fixes jsdom tests

--- template/tests/jest.config.js
+++ template/tests/jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
   // without this line rootDir will be `./tests`
   rootDir: '..',
 
+  // without this line sometimes `jsdom` fails to run the tests:
+  // see: https://github.com/facebook/jest/issues/6766
+  testURL: 'http://localhost',
+
   collectCoverage: true,
   coverageThreshold: {
     global: { statements: 90 }
